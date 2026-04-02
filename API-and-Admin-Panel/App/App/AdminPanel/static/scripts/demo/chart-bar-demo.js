35f190592597b63ecadf78f7476d30c4afa81e4e
Kavia AI <production@kavia.ai>
2026-04-02 09:31:42 +0000

    Checkpoint 3 - cm417e8c30

--- API-and-Admin-Panel/App/App/AdminPanel/static/scripts/demo/chart-bar-demo.js
+++ API-and-Admin-Panel/App/App/AdminPanel/static/scripts/demo/chart-bar-demo.js
@@ -1,37 +1,8 @@
-// Set new default font family and font color to mimic Bootstrap's default styling
-Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
-Chart.defaults.global.defaultFontColor = '#858796';
-
-function number_format(number, decimals, dec_point, thousands_sep) {
-  // *     example: number_format(1234.56, 2, ',', ' ');
-  // *     return: '1 234,56'
-  number = (number + '').replace(',', '').replace(' ', '');
-  var n = !isFinite(+number) ? 0 : +number,
-    prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
-    sep = (typeof thousands_sep === 'undefined') ? ',' : thousands_sep,
-    dec = (typeof dec_point === 'undefined') ? '.' : dec_point,
-    s = '',
-    toFixedFix = function(n, prec) {
-      var k = Math.pow(10, prec);
-      return '' + Math.round(n * k) / k;
-    };
-  // Fix for IE parseFloat(0.55).toFixed(0) = 0;
-  s = (prec ? toFixedFix(n, prec) : '' + Math.round(n)).split('.');
-  if (s[0].length > 3) {
-    s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep);
-  }
-  if ((s[1] || '').length < prec) {
-    s[1] = s[1] || '';
-    s[1] += new Array(prec - s[1].length + 1).join('0');
-  }
-  return s.join(dec);
-}
-
 var labels = [];
 var revenue = [];
 var receipts = [];
 var largest = 0;
-$.getJSON("http://soc-web-liv-82.napier.ac.uk/api/money", function(result){
+$.getJSON("/api/money", function(result){
     for (var i=0; i<result.length; i++)
     {
       labels.push(result[i].Month);
@@ -100,7 +71,7 @@ var myBarChart = new Chart(ctx, {
           padding: 10,
           // Include a pound sign in the ticks
           callback: function(value, index, values) {
-            return '£' + number_format(value);
+            return '\u00a3' + number_format(value);
           }
         },
         gridLines: {
@@ -130,11 +101,10 @@ var myBarChart = new Chart(ctx, {
       callbacks: {
         label: function(tooltipItem, chart) {
           var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
-          return datasetLabel + ': £' + number_format(tooltipItem.yLabel);
+          return datasetLabel + ': \u00a3' + number_format(tooltipItem.yLabel);
         }
       }
     },
   }
 });
-
 }
